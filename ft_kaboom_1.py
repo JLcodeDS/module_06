402880d305ed50ea3bad1d04056a99420802d20d
JLcodeDS <jonathan.ludolph@gmail.com>
2026-07-17 19:05:10 +0200

    first commit

---
+++ ft_kaboom_1.py
@@ -0,0 +1,8 @@
+import alchemy.grimoire.dark_spellbook as dark_spellbook
+
+
+if __name__ == "__main__":
+    print("=== KABOOM 1 ===")
+    print("Accessing dark spellbook")
+    print("Testing import now - this will cause an error:")
+    print(dark_spellbook.dark_spell_record("dark spell", "toad, frogs"))
