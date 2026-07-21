a0d2766961f6069643d11aed6ff62d03321ee8b7
JLcodeDS <jonathan.ludolph@gmail.com>
2026-07-21 14:52:49 +0200

    fixed inits

--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -1,8 +1,9 @@
-import alchemy.grimoire.dark_spellbook as dark_spellbook
-
-
 if __name__ == "__main__":
     print("=== KABOOM 1 ===")
     print("Accessing dark spellbook")
-    print("Testing import now - this will cause an error:")
-    print(dark_spellbook.dark_spell_record("dark spell", "toad, frogs"))
+    print("Testing import now - THIS WILL CAUSE AN ERROR:")
+    try:
+        import alchemy.grimoire.dark_spellbook as dark_spellbook
+        print(dark_spellbook.dark_spell_record("dark spell", "toad, frogs"))
+    except Exception as e:
+        print(f"Error importing with circular dependency: {e}")
